70c828f15c281622daeddceb20315ef9f7244510
taran-1807 <88237262+taran-1807@users.noreply.github.com>
2022-06-25 21:55:28 +0530

    Create printLikeAWave.cpp

---
+++ printLikeAWave.cpp
@@ -0,0 +1,21 @@
+vector<int> wavePrint(vector<vector<int>> arr, int nRows, int mCols)
+{
+    vector<int> ans;
+    
+    for(int col = 0; col<mCols; col++){
+        
+        if(col&1){
+            //odd index --> bottom to top
+            for(int row = nRows-1; row>=0; row--){
+                ans.push_back(arr[row][col]);
+            }
+        }
+        else{
+            //0 or even index --> top to bottom
+            for(int row = 0; row<nRows; row++){
+                ans.push_back(arr[row][col]);
+            }
+        }
+    }
+    return ans;
+}
